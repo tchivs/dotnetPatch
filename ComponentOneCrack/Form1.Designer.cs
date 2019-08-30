c81fce0e6365fd315f9f76421b3fb70109ebcd0a
tchivs <topivn@live>
2019-08-30 18:16:09 +0800

    add more

--- ComponentOneCrack/Form1.Designer.cs
+++ ComponentOneCrack/Form1.Designer.cs
@@ -32,6 +32,9 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -68,6 +71,33 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(90, 96);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(358, 21);
+            this.textBox3.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(476, 93);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Check2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(557, 94);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "原版";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -75,6 +105,9 @@
             this.ClientSize = new System.Drawing.Size(717, 218);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
@@ -91,6 +124,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- ComponentOneCrack/Form1.cs
+++ ComponentOneCrack/Form1.cs
@@ -6,6 +6,8 @@ using System;
 using System.IO;
 using System.Reflection;
 using System.Windows.Forms;
+using dnlib.DotNet.Resources;
+using dnlib.W32Resources;
 using tchivs.patch;
 using OpCodes = dnlib.DotNet.Emit.OpCodes;
 
@@ -17,6 +19,7 @@ namespace ComponentOneCrack
         {
             InitializeComponent();
             this.textBox1.Text = @"D:\Program Files (x86)\ComponentOne\WinForms Edition\bin\v4.5.2\C1.Win.C1List.4.5.2.dll";
+            this.textBox3.Text = @"C:\Users\tchivs\Source\Repos\tchivs\dotnetPatch\TestLibrary\bin\Debug\TestLibrary.dll";
         }
 
         private void Button1_Click(object sender, EventArgs e)
@@ -36,26 +39,23 @@ namespace ComponentOneCrack
             int i = 0;
             foreach (MethodDef method in type.Methods)
             {
-                if (method.Body.Instructions.Count == 143+1)
+                if (method.Body.Instructions.Count == 143 + 1)
                 {
 
-                        method.Body.Instructions[43] = Instruction.CreateLdcI4(0);
-                        Console.WriteLine("ValidateRuntime Crack Success");
-                        i++;
-                    
-                    
+                    method.Body.Instructions[43] = Instruction.CreateLdcI4(0);
+                    Console.WriteLine("ValidateRuntime Crack Success");
+                    i++;
                 }
-                else if (method.Body.Instructions.Count == 28+1)
+                else if (method.Body.Instructions.Count == 28 + 1)
                 {
                     method.Body.Instructions[6] = Instruction.CreateLdcI4(0);
                     Console.WriteLine("ValidateDesigntime Crack Success");
                     i++;
                 }
-
-                if (i==2)
+                if (i == 2)
                 {
                     break;
-                    
+
                 }
                 Console.WriteLine(method.Name);
 
@@ -130,6 +130,55 @@ namespace ComponentOneCrack
             {
                 MetadataOptions = { Flags = MetadataFlags.KeepOldMaxStack }
             });
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            string file = this.textBox3.Text;
+            Patcher patcher = new Patcher(file);
+            var res = patcher.Module.Resources.FindEmbeddedResource("app.resources").CreateReader();
+            
+            ResourceElement element = res.ResourceElements.First(s => s.Name == resxName);
+            MemoryStream reStream = new MemoryStream();
+            res.CreateReader().CopyTo(reStream);
+
+
+
+          var rsA=  Assembly.Load(reStream.ToArray());
+
+            PatchInfo patchInfo = new PatchInfo()
+            {
+                Namespace = "TestLibrary",
+                Class = "TestAction",
+                Method = "Check",
+            };
+            // var methodDef = patcher.FindMethod(patchInfo);
+            patcher.WriteReturnBody(patchInfo, true);
+            AssemblyDef assemblyDef = patcher.Module.Assembly;
+            MemoryStream memoryStream = new MemoryStream();
+            patcher.Module.Write(memoryStream, new ModuleWriterOptions(patcher.Module) { MetadataOptions = { Flags = MetadataFlags.KeepOldMaxStack } });
+
+            Assembly assembly = Assembly.Load(memoryStream.ToArray());
+
+            Type type = assembly.GetType("TestLibrary.TestAction");
+            MethodInfo method = type.GetMethod("Check");
+            var ins = Activator.CreateInstance(type);
+            var parameters = new object[] { this.textBox2.Text };
+            bool result = (bool)method.Invoke(ins, parameters);
+            MessageBox.Show(result.ToString());
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            string file = this.textBox3.Text;
+            Assembly assembly = Assembly.LoadFile(file);
+
+            Type type = assembly.GetType("TestLibrary.TestAction");
+            MethodInfo method = type.GetMethod("Check");
+            var ins = Activator.CreateInstance(type);
+            var parameters = new object[] { this.textBox2.Text };
+            bool result = (bool)method.Invoke(ins, parameters);
+            MessageBox.Show(result.ToString());
         }
     }
 }
